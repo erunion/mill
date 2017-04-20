bc5d4905e4c92edaaa0ee7be4b6c2aa923b3299a
Jon Ursenbach <jonu@vimeo.com>
2017-04-20 11:48:32 -0400

    Adding Content-Type headers into generated Blueprint files. #40

--- src/Generator/Blueprint.php
+++ src/Generator/Blueprint.php
@@ -236,6 +236,16 @@ class Blueprint extends Generator
         $blueprint = '+ Request';
         $blueprint .= $this->line();
 
+        // Build up request headers.
+        $blueprint .= $this->tab();
+        $blueprint .= '+ Headers';
+        $blueprint .= $this->line(2);
+
+        $blueprint .= $this->tab(2);
+        $blueprint .= sprintf('Content-Type: %s', $action->getContentType());
+        $blueprint .= $this->line(2);
+
+        // Build up request attributes.
         $blueprint .= $this->tab();
         $blueprint .= '+ Attributes';
         $blueprint .= $this->line();
